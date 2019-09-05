c9211ae064c4401e8b2eebd4100595c80c8c8283
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2019-09-05 06:48:18 +0500

    Update pritunl.sh

--- pritunl.sh
+++ pritunl.sh
@@ -14,7 +14,12 @@ service docker start
 chkconfig docker on
 
 
-docker run --name pritunl --privileged -v ~/pritunl/mondodb:/var/lib/mongodb -v ~/pritunl/pritunl:/var/lib/pritunl -v ~/pritunl/etc:/etc -p 12323:1194/udp -p 12323:1194/tcp -p 80:80/tcp -p 443:443/tcp --restart unless-stopped -d jippi/pritunl
+docker volume create pritunl-conf
+docker volume ls
+
+docker run --name pritunl --privileged -v ~/pritunl/mondodb:/var/lib/mongodb -v ~/pritunl/pritunl:/var/lib/pritunl -v pritunl-conf:/etc -p 12323:1194/udp -p 12323:1194/tcp -p 80:80/tcp -p 443:443/tcp --restart unless-stopped -d jippi/pritunl
+
+
 
 # Username & Password ---> Start
 # Username: pritunl Password: pritunl
