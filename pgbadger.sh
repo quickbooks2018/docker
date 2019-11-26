5f021793f7aec0f8622e56b7f39caf692d3b9412
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2019-11-26 07:02:07 +0500

    pgbadger-report cronjob & latest report

--- pgbadger.sh
+++ pgbadger.sh
@@ -5,7 +5,15 @@
 
 # Log file name ---> /cartodb-storage/log/postgresql/postgresql-9.3-main.log
 
+
+# To Setup a cron job & fresh reports,I am removing the exisitng setup of container pgbadger-report
+
+docker rm -f pgbadger-report 2>&1 >/dev/null
+
+rm -rf /root/pgbadger 2>&1 >/dev/null
+
 POSTGRESQL_LOGS_DIR=/cartodb-storage/log/postgresql
+IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 
 export POSTGRESQL_LOGS_DIR
 
@@ -22,6 +30,10 @@ mv /root/pgbadger/out.html /root/pgbadger/index.html
 docker rm -f pgbadger
 
 docker run --name pgbadger-report -v /root/pgbadger:/usr/local/apache2/htdocs -p 10000:80 -d httpd
+
+echo ""
+
+echo "ALHUMDULLIAH Latest pgbadger-report is available at "$IP":10000"
 
 # docker rm -f pgbadger-report
 
