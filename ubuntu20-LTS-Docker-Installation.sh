eae050ecc57482aa735a8fed14f45059a8e07753
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2021-02-26 10:06:35 +0500

    Create ubuntu20-LTS-Docker-Installation.sh

---
+++ ubuntu20-LTS-Docker-Installation.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+apt update -y
+
+apt install apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+apt update -y
+
+apt-cache policy docker-ce
+
+apt install docker-ce
+
+usermod -aG docker ${USER}
+
+# usermod -aG docker username
+
+systemctl restart docker
+
+systemctl status docker
+
+exit 0
+
+#END
+
+
+
+
