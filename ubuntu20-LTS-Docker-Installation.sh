21c936982ebcf930b7d9b0d1afc293da6c91e3cf
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2021-03-02 07:32:42 +0500

    Update ubuntu20-LTS-Docker-Installation.sh

--- ubuntu20-LTS-Docker-Installation.sh
+++ ubuntu20-LTS-Docker-Installation.sh
@@ -28,7 +28,7 @@ systemctl enable docker
 # RUN Docker with out sudo
 
 sudo setfacl -m user:$USER:rw /var/run/docker.sock
-
+# https://askubuntu.com/questions/477551/how-can-i-use-docker-without-sudo#:~:text=If%20you%20don't%20want,writable%20by%20the%20docker%20group.&text=Either%20do%20a%20newgrp%20docker,activate%20the%20changes%20to%20groups.
 exit 0
 
 #END
