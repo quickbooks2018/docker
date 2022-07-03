8eb4e525efd5b8fe81b6043df271ec001d881c0d
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2022-07-03 16:07:25 +0500

    Update Pri-Tunnel.sh

--- Pri-Tunnel.sh
+++ Pri-Tunnel.sh
@@ -20,6 +20,9 @@ docker volume ls
 
 docker run --name pritunl --privileged -v ~/pritunl/mondodb:/var/lib/mongodb -v ~/pritunl/pritunl:/var/lib/pritunl -v pritunl-conf:/etc -p 12323:1194/udp -p 12323:1194/tcp -p 80:80/tcp -p 443:443/tcp --restart unless-stopped -d jippi/pritunl
 
+docker network pritunl
+
+docker run --name pritunl --privileged -e MONGO_URI="mongodb://mongodb:27017/pritunl-zero" -v pritunl:/var/lib/pritunl -v pritunl-conf:/etc -p 12323:1194/udp -p 12323:1194/tcp -p 80:80/tcp -p 443:443/tcp --restart unless-stopped -d docker.io/pritunl/pritunl-zero:latest
 # Username & Password ---> Start
 # Username: pritunl Password: pritunl
 
