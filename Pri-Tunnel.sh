d6fd7d1b2d1554e20c77bbc494af114b473dcfcd
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2022-07-03 19:00:17 +0500

    Update Pri-Tunnel.sh

--- Pri-Tunnel.sh
+++ Pri-Tunnel.sh
@@ -15,10 +15,13 @@ systemctl start docker
 systemctl enable docker
 rm -rf get-docker.sh
 
-docker volume create pritunl-conf
+# docker volume create pritunl-conf
 docker volume ls
 
-docker run --name pritunl --privileged -v ~/pritunl/mondodb:/var/lib/mongodb -v ~/pritunl/pritunl:/var/lib/pritunl -v pritunl-conf:/etc -p 12323:1194/udp -p 12323:1194/tcp -p 80:80/tcp -p 443:443/tcp --restart unless-stopped -d jippi/pritunl
+# docker run --name pritunl --privileged -v ~/pritunl/mondodb:/var/lib/mongodb -v ~/pritunl/pritunl:/var/lib/pritunl -v pritunl-conf:/etc -p 12323:1194/udp -p 12323:1194/tcp -p 80:80/tcp -p 443:443/tcp --restart unless-stopped -d jippi/pritunl
+
+docker run --name pritunl --privileged mondodb:/var/lib/mongodb -v pritunl:/var/lib/pritunl -v pritunl-conf:/etc -p 12323:1194/tcp -p 443:443/tcp --restart unless-stopped -id jippi/pritunl:latest
+
 
 # https://hub.docker.com/u/pritunl
 
