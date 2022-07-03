c09162bb4190242071fa0660d4dc77d16413d3c8
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2022-07-03 17:29:12 +0500

    Update Pri-Tunnel.sh

--- Pri-Tunnel.sh
+++ Pri-Tunnel.sh
@@ -20,13 +20,13 @@ docker volume ls
 
 docker run --name pritunl --privileged -v ~/pritunl/mondodb:/var/lib/mongodb -v ~/pritunl/pritunl:/var/lib/pritunl -v pritunl-conf:/etc -p 12323:1194/udp -p 12323:1194/tcp -p 80:80/tcp -p 443:443/tcp --restart unless-stopped -d jippi/pritunl
 
-# https://github.com/myoung34/docker-pritunl
+# https://hub.docker.com/u/pritunl
 
 # docker network create pritunl --attachable
 
 # https://hub.docker.com/_/mongo
 
-docker run --name mongodb -e MONGO_INITDB_ROOT_USERNAME=mongoadmin -e MONGO_INITDB_ROOT_PASSWORD=secret -e MONGO_INITDB_DATABASE=pritunl --network pritunl -v mongodb:/data/db --restart unless-stopped -id mongo:latest
+# docker run --name mongodb -e MONGO_INITDB_ROOT_USERNAME=mongoadmin -e MONGO_INITDB_ROOT_PASSWORD=secret -e MONGO_INITDB_DATABASE=pritunl --network pritunl -v mongodb:/data/db --restart unless-stopped -id mongo:latest
 
 # mongo --username mongoadmin --password --authenticationDatabase admin --host localhost --port 27017
 
